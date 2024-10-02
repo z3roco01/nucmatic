bb79db5abf8fbea0811da99b0d988e2caa975297
z3roco01 <zerocool@e.email>
2024-10-02 09:34:49 -0500

    make EffectArmourItem apply the effect
    new video soon - drooids

--- src/main/kotlin/z3roco01/nucmatic/item/EffectArmourItem.kt
+++ src/main/kotlin/z3roco01/nucmatic/item/EffectArmourItem.kt
@@ -1,6 +1,7 @@
 package z3roco01.nucmatic.item
 
 import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.item.ArmorItem
 import net.minecraft.item.ArmorMaterial
@@ -20,7 +21,7 @@ import z3roco01.nucmatic.Nucmatic
 class EffectArmourItem(material: RegistryEntry<ArmorMaterial>, type: Type, settings: Settings,
                        val effect: StatusEffectInstance): ArmorItem(material, type, settings) {
     override fun inventoryTick(stack: ItemStack, world: World, entity: Entity, slot: Int, selected: Boolean) {
-        if(slot == type.equipmentSlot.entitySlotId)
-            Nucmatic.LOGGER.info("pppppp")
+        if(slot == type.equipmentSlot.entitySlotId && entity is LivingEntity)
+            entity.addStatusEffect(effect)
     }
 }
