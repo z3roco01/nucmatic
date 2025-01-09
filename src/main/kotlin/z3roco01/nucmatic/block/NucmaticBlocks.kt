10a4906fa9b3f40a908a86fb4701dd016f2e240e
z3roco01 <zerocool@e.email>
2025-01-09 09:24:32 -0600

    add generator casing, glass and generator controller

--- src/main/kotlin/z3roco01/nucmatic/block/NucmaticBlocks.kt
+++ src/main/kotlin/z3roco01/nucmatic/block/NucmaticBlocks.kt
@@ -8,6 +8,7 @@ import net.minecraft.item.Item
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import z3roco01.nucmatic.Nucmatic
+import z3roco01.nucmatic.block.entity.NuclearGeneratorControllerBlock
 
 /**
  * contains all the block for this mod
@@ -15,9 +16,12 @@ import z3roco01.nucmatic.Nucmatic
  */
 object NucmaticBlocks {
     // all the blocks in the mod
-    val URANIUM_ORE           = Block(Settings.copy(Blocks.IRON_ORE))
-    val DEEPSLATE_URANIUM_ORE = Block(Settings.copy(Blocks.IRON_ORE))
-    val NUCLEAR_GENERATOR    = NuclearGeneratorBlock()
+    val URANIUM_ORE                  = Block(Settings.copy(Blocks.IRON_ORE))
+    val DEEPSLATE_URANIUM_ORE        = Block(Settings.copy(Blocks.IRON_ORE))
+    val GENERATOR_CASING             = Block(Settings.create().strength(5f, 6f))
+    val GENERATOR_GLASS              = Block(Settings.copy(Blocks.GLASS).strength(4.5f, 5.5f))
+    val NUCLEAR_GENERATOR_CONTROLLER = NuclearGeneratorControllerBlock()
+    val NUCLEAR_GENERATOR            = NuclearGeneratorBlock()
 
     /**
      * registers all the blocks for this mod
@@ -25,6 +29,9 @@ object NucmaticBlocks {
     fun register() {
         register("uranium_ore", URANIUM_ORE)
         register("deepslate_uranium_ore", DEEPSLATE_URANIUM_ORE)
+        register("generator_casing", GENERATOR_CASING)
+        register("generator_glass", GENERATOR_GLASS)
+        register("generator_controller", NUCLEAR_GENERATOR_CONTROLLER)
         register("nuclear_generator", NUCLEAR_GENERATOR)
     }
 
---
+++ src/main/kotlin/z3roco01/nucmatic/block/entity/NuclearGeneratorControllerBlock.kt
@@ -0,0 +1,6 @@
+package z3roco01.nucmatic.block.entity
+
+import net.minecraft.block.Block
+
+class NuclearGeneratorControllerBlock: Block(Settings.create()) {
+}
